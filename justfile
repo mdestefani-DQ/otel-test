a0d942b72775dba91b13b7c9890016fa0161632b
mdestefani-DQ <marco.destefani@donq.io>
2025-05-05 14:23:07 +0200

    fix: minor changes

--- justfile
+++ justfile
@@ -2,3 +2,9 @@ default: dev
 
 dev:
   @mprocs "npm run start:dev"
+
+build:
+  @mprocs "npm run build"
+
+run: build
+  @mprocs "npm run start:prod"
--- src/tracing.ts
+++ src/tracing.ts
@@ -4,13 +4,17 @@ import { NodeSDK } from "@opentelemetry/sdk-node";
 import { BatchLogRecordProcessor } from '@opentelemetry/sdk-logs';
 import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http';
 
+// For troubleshooting, set the log level to DiagLogLevel.DEBUG
+// import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+
 const traceExporter = new OTLPTraceExporter()
 const batchRecordProcessor = new BatchLogRecordProcessor(new OTLPLogExporter())
 
 const sdk = new NodeSDK({
   traceExporter,
   logRecordProcessors: [batchRecordProcessor],
-  instrumentations: [getNodeAutoInstrumentations()]
+  instrumentations: [getNodeAutoInstrumentations()],
 });
 
 process.on("SIGTERM", () => {
